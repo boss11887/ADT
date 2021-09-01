bded3fe14cab3f062e5f7594b6416c8e7b97ab75
boss11887 <33756509+boss11887@users.noreply.github.com>
2021-09-01 10:11:31 +0700

    Delete Week1 directory

--- Week1/1.waiting.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int q,w;
-    scanf("%d %d",&q,&w);
-    printf("%d %d",(q+w)/24,(q+w)%24);
-    return 0;
-}
--- Week1/3.toilet.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int new,old,c,n;
-    scanf("%d %d %d %d",&new,&old,&c,&n);
-    printf("%d",1000*c/(n*(old-new)) + ((1000*c)%(n*(old-new)) != 0 ) );
-    return 0;
-}
-//2 15 150 14
